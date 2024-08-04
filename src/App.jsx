769f5fed4e320dabd567a36838571a199e53c03a
Shivam Pate <shivampate333@gmail.com>
2024-08-04 23:29:45 +0530

    created signup api route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 
+import { BrowserRouter ,Route,Routes } from 'react-router-dom'
 
-import './App.css'
+import About from './pages/About'
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import Signout from './pages/Signout'
+import Header from './commponent/Header'
 
 function App() {
   
 
   return (
     <>
-      <div className='bg-red-400'>rfyhryhr</div>
+    
+       <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<Signin />} />
+        <Route path='/sign-out' element={<Signout />} />
+        <Route path='/about' element={<About />} />
+        
+
+      </Routes>
+    </BrowserRouter>
        
     </>
   )
