0c567bb006cc79db1e492890f2aa343c4c95755a
shivam pate <137706111+Shivam-Pate@users.noreply.github.com>
2024-08-10 15:35:17 +0530

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+import authRouter from '../api/routes/auth.route.js'
+import cookieParser from 'cookie-parser';
+import userRouter from './routes/user.route.js';
+import listingRouter from './routes/listing.route.js'
+
+
+dotenv.config({ path: './api/.env' });
+
+
+   mongoose.connect(process.env.MONGO)
+  .then(() => {
+  console.log('Connected to MongoDB!');
+})
+.catch((err) => {
+  console.log(err);
+});
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cookieParser())
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000!ujg');
+  });
+
+  app.use('/api/auth', authRouter);
+  app.use('/api/user', userRouter);
+  app.use('/api/listing', listingRouter);
+
+
+
+  app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+  });
